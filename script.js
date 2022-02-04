53de943ac24429e7c00f29ec2c14df5aba8e1a58
Valera <example@gmail.com>
2022-02-04 19:09:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// console.log(second);
+// const pi = 3.14;
+
+var number = 5;
+var string = 'Hello!';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+
+
+
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert('Hello World!');
+
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('You are 18 years old?', 'Yes');
+
+// console.log(typeof(answer));
+
+// console.log('arr' + '- object');
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
+
